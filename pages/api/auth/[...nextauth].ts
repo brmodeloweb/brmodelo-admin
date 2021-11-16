93edd6b6e9cf838261dae899381679cb1c9600c0
Vinicius <viniciusvolpe44@gmail.com>
2021-11-16 09:35:46 +0000

    Configuring Github auth provider

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+export default NextAuth({
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      console.log(user, account, profile);
+      return true;
+    },
+  },
+});
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 export default function Home() {
+  const [session] = useSession();
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
@@ -9,13 +11,32 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to Br Modelo Admin
-        </h1>
+        {session ? (
+          <>
+            <h1>Signed in as {session.user.name}</h1>
+            <button
+              onClick={() => {
+                signOut();
+              }}
+            >
+              Sign out
+            </button>
+          </>
+        ) : (
+          <>
+            <h1>Welcome to Br Modelo Admin</h1>
+            <button
+              onClick={() => {
+                signIn();
+              }}
+            >
+              Sign in
+            </button>
+          </>
+        )}
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-      </footer>
+      <footer className="flex items-center justify-center w-full h-24 border-t"></footer>
     </div>
-  )
+  );
 }
